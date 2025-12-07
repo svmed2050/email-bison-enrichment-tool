16af81280ae5a0774203edf4b0af7e418e753018
svmed2050 <pavelsergienko7@gmail.com>
2025-12-07 15:14:44 +0200

    Add Account Name and LinkedIn Profile columns support

--- config.py
+++ config.py
@@ -7,12 +7,16 @@ DEFAULT_PATH = os.path.join(os.path.expanduser('~'), 'Desktop')
 # Column mapping (Case insensitive search)
 PERSON_FIRST_NAME_COLS = ['First Name', 'FirstName', 'First']
 PERSON_LAST_NAME_COLS = ['Last Name', 'LastName', 'Last']
-PERSON_LINKEDIN_COLS = ['Person Linkedin Url', 'Personal LinkedIn', 'Linkedin', 'Person Linkedin']
-COMPANY_NAME_COLS_CLAY = ['FINAL Company Name', 'Company Name', 'Company', 'Organization']
-COMPANY_NAME_COLS_EB = ['Company', 'Company Name', 'Account']
+
+# UPDATED: Added 'LinkedIn Profile URL', 'Linkedin Profile Url'
+PERSON_LINKEDIN_COLS = ['Person Linkedin Url', 'Personal LinkedIn', 'Linkedin', 'Person Linkedin', 'LinkedIn Profile URL', 'Linkedin Profile Url'] 
+
+# UPDATED: Added 'Account Name'
+COMPANY_NAME_COLS_CLAY = ['FINAL Company Name', 'Company Name', 'Company', 'Organization', 'Account Name', 'Account']
+COMPANY_NAME_COLS_EB = ['Company', 'Company Name', 'Account', 'Account Name']
 COMPANY_LINKEDIN_COLS = ['Company Linkedin Url', 'Company Linkedin', 'Organization Linkedin Url']
 
-# Additional columns for DNC and Output
+# Additional columns
 EMAIL_COLS = ['Email', 'Work Email', 'Contact Email', 'Email Address']
 TITLE_COLS = ['Title', 'Job Title', 'Position']
 WEBSITE_COLS = ['Website', 'Website URL', 'Company Domain Name', 'Domain', 'Company Website']
